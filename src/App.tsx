6ff6b2e6831f50c3e3134d339f1e01704c03b2f2
Moita <katakenshi@icloud.com>
2022-11-25 11:34:50 -0300

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      Our Pricing
+      Annually
+      Monthly
+
+      Basic
+      &dollar;19.99
+      &dollar;199.99
+      500 GB Storage
+      2 Users Allowed
+      Send up to 3 GB
+      Learn More
+
+      Professional
+      &dollar;24.99
+      &dollar;249.99
+      1 TB Storage
+      5 Users Allowed
+      Send up to 10 GB
+      Learn More
+
+      Master
+      &dollar;39.99
+      &dollar;399.99
+      2 TB Storage
+      10 Users Allowed
+      Send up to 20 GB
+      Learn More
+      <div className="attribution">
+        Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
+        Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  )
+}
+
+export default App
